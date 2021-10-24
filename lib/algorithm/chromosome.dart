68b92ecb2feb6abcae7423cf4d125a3cc79b4edd
h80r <h80rgbc@gmail.com>
2021-10-24 11:02:44 -0300

    Update fitness using Grid and Cells

--- lib/algorithm/chromosome.dart
+++ lib/algorithm/chromosome.dart
@@ -1,58 +1,53 @@
 import 'dart:math';
 
 import 'package:genetic_sudoku/algorithm/gene.dart';
+import 'package:genetic_sudoku/models/cell.dart';
+import 'package:genetic_sudoku/models/grid.dart';
 
 class Chromosome {
   Chromosome() {
     genes = List.filled(81, Gene());
+    grid = Grid.fromChromosome(this);
   }
 
   late final List<Gene> genes;
+  late final Grid grid;
   late int fitness;
 
   void applyFitness() {
-    var mappedGenes = List.generate(
-      genes.length,
-      (i) => {
-        'gene': genes[i],
-        'position': Point(i % 9, i ~/ 9),
-        'quadrant': [
-          [1, 2, 3],
-          [4, 5, 6],
-          [7, 8, 9]
-        ][(i ~/ 9) ~/ 3][(i % 9) ~/ 3],
-      },
-    );
+    void evaluateCell({
+      required Cell target,
+      required List<Cell> line,
+      required List<Cell> column,
+      required List<Cell> square,
+    }) {
+      final lineCopies = line.where((e) => e.value == target.value).length;
+      final columnCopies = column.where((e) => e.value == target.value).length;
+      final squareCopies = square.where((e) => e.value == target.value).length;
 
-    fitness = mappedGenes.map((mappedGene) {
-      final mappedGeneLine = mappedGenes.where((gene) =>
-          (gene['position'] as Point).y == (mappedGene['position'] as Point).y);
-      final mappedGeneColumn = mappedGenes.where((gene) =>
-          (gene['position'] as Point).x == (mappedGene['position'] as Point).x);
-      final mappedGeneQuadrant = mappedGenes.where((gene) =>
-          (gene['quadrant'] as int) == (mappedGene['quadrant'] as int));
+      target.copiesInRange = lineCopies + columnCopies + squareCopies - 2;
+      target.validShapes = [
+        lineCopies,
+        columnCopies,
+        squareCopies,
+      ].where((e) => e == 1).length;
+    }
 
-      final copiesInLine = mappedGeneLine
-          .where((e) =>
-              (e['gene'] as Gene).value == (mappedGene['gene'] as Gene).value)
-          .length;
-      final copiesInColumn = mappedGeneColumn
-          .where((e) =>
-              (e['gene'] as Gene).value == (mappedGene['gene'] as Gene).value)
-          .length;
-      final copiesInQuadrant = mappedGeneQuadrant
-          .where((e) =>
-              (e['gene'] as Gene).value == (mappedGene['gene'] as Gene).value)
-          .length;
+    final cells = grid.cells;
 
-      final copiesInRange =
-          copiesInLine + copiesInColumn + copiesInQuadrant - 2;
-      final validShapes = [copiesInLine, copiesInColumn, copiesInQuadrant]
-          .where((e) => e == 1)
-          .length;
+    for (final cell in cells) {
+      evaluateCell(
+        target: cell,
+        line: cells.where((e) => e.position.y == cell.position.y).toList(),
+        column: cells.where((e) => e.position.x == cell.position.x).toList(),
+        square: cells.where((e) => e.square == cell.square).toList(),
+      );
+    }
 
-      return pow(3, validShapes) - pow(2, copiesInRange) as int;
-    }).reduce((total, element) => total + element);
+    final fitnessPerGene = grid.cells
+        .map((e) => pow(3, e.validShapes) - pow(2, e.copiesInRange) as int);
+
+    fitness = fitnessPerGene.reduce((a, b) => a + b);
   }
 
   @override
