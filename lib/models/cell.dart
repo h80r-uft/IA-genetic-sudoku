52c0878111e0d2f564bf009afa1ce862b319fbce
h80r <h80rgbc@gmail.com>
2021-10-27 09:45:27 -0300

    Add `cell.dart` documentation

--- lib/models/cell.dart
+++ lib/models/cell.dart
@@ -1,13 +1,47 @@
 import 'dart:math';
 
+/// É a representação visual de um [Gene].
+///
+/// Pode ser observada como cada célula do tabuleiro do sudoku.
+///
+/// * [value] é o valor da célula.
+/// * [position] é a posição da célula no tabuleiro.
+/// * [copiesInRange] é a quantidade de cópias no alcance da célula atual.
+/// * [validShape] é a quantidade de formas válidas das quais a célula
+/// participa.
 class Cell {
+  /// Gera uma nova célula com [value] e [position] informados.
   Cell({required this.value, required this.position});
 
+  /// Armazena o valor atual da célula.
+  ///
+  /// Pode ser qualquer número no intervalo `[1 ... 9]`.
   final int value;
+
+  /// Armazena a posição da célula atual.
+  ///
+  /// Utiliza a classe [Point] que possui as coordenadas `x` e `y`.
+  ///
+  /// A primeira célula do tabuleiro de sudoku teria [position] = `Point(0, 0)`.
+  /// Já a última seria `Point(8, 8)`.
   final Point position;
+
+  /// Armazena quantas células de valor [value] estão presentes no alcance da
+  /// célula.
+  ///
+  /// Inclui a célula atual.
+  ///
+  /// O alcance seria a linha, a coluna e o quadrado dos quais a célula
+  /// participa.
   var copiesInRange = 1;
+
+  /// Armazena quantas formas concluídas a célula faz parte.
+  ///
+  /// Formas concluídas seriam linhas, colunas e quadrados em que não há
+  /// repetição de valor.
   var validShapes = 0;
 
+  /// Retorna em qual quadrado a célula se encontra.
   int get square => [
         [1, 2, 3],
         [4, 5, 6],
